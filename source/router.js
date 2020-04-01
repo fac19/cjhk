125bf2bd91b208ff1d67c9d5fed844d68c7cd909
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-04-01 18:39:28 +0100

    Made barebones server Relates #5
    Co-authored-by: Jack Heriz-Smith <jackherizsmith@gmail.com>

---
+++ source/router.js
@@ -0,0 +1,18 @@
+const handlers = require("./handlers");
+
+function router(request, response) {
+  const { url, method } = request;
+  if (url === "/") {
+    handlers.homeHandler(request, response);
+  } else if (url.includes("public")) {
+    handlers.publicHandler(request, response);
+  } else if (url === "/add" && method == "GET") {
+    handlers.addPageHandler(request, response);
+  } else if (url === "/add" && method == "POST") {
+    handlers.addToolHandler(request, response);
+  } else {
+    handlers.missingHandler(request, response);
+  }
+}
+
+module.exports = router;
