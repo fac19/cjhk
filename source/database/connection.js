ceaff8ef865f109d816949836b495fce86254921
AlexReid95 <alexreid95@gmail.com>
2020-04-03 11:55:32 +0100

    Fix connectionString

--- source/database/connection.js
+++ source/database/connection.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const db = new pg.Pool(
-    connectionString: process.env.DATABASE_URL
+const db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
     // if we have a database URL (e.g. from Heroku we'll use that)
     // otherwise it'll default to your local .env variables
-);
+});
 
 module.exports = db;
